3abe0891847de2791b94c400e6646f3e4ed52ae9
Ravi4559 <165661246+Ravi4559@users.noreply.github.com>
2025-08-29 18:51:44 +0530

    AuthDtos.cs

--- Contracts/AuthDtos.cs
+++ Contracts/AuthDtos.cs
@@ -4,11 +4,10 @@ namespace LotusAscend.Contracts
 {
     public class AuthDtos
     {
-        // Contracts/AuthDtos.cs
-
-
-public record RegisterRequest([Required] string MobileNumber);
-    public record VerifyRequest([Required] string MobileNumber, [Required] string Otp);
-    public record AuthResponse(int MemberId, string MobileNumber, string Token);
-}
+    
+        public record RegisterRequest([Required] string MobileNumber);
+        public record VerifyRequest([Required] string MobileNumber, [Required] string Otp);
+         public record AuthResponse(int MemberId, string MobileNumber, string Token);
+   
+    }
 }
