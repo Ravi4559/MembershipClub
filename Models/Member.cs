fc66aa5f1386a15aa3d27a841facaa78af2fddcc
Ravi4559 <165661246+Ravi4559@users.noreply.github.com>
2025-08-29 18:46:10 +0530

    Member.cs

--- Models/Member.cs
+++ Models/Member.cs
@@ -1,6 +1,6 @@
 ﻿namespace LotusAscend.Models
 {
-    // Models/Member.cs
+    
     public class Member
     {
         public int Id { get; set; }
@@ -8,7 +8,6 @@
         public bool IsVerified { get; set; } = false;
         public int Points { get; set; } = 0;
 
-        // Navigation properties
         public Otp? Otp { get; set; }
         public ICollection<PointTransaction> PointTransactions { get; set; } = new List<PointTransaction>();
     }
